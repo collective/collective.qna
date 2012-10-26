187f96086ece761f870f9ae78ce787d60406dd94
Jamie Lentin <jm@lentin.co.uk>
2012-10-26 12:28:02 +0100

    Add a vocabulary for the answers
    However, is there much point to doing so?

--- collective/qna/behaviors.py
+++ collective/qna/behaviors.py
@@ -1,7 +1,9 @@
-from zope.interface import implements, alsoProvides
+import re
 
 from zope import schema
-from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+from zope.interface import implements, alsoProvides
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
 
 from z3c.form.interfaces import IDisplayForm
 from z3c.form.object import registerFactoryAdapter
@@ -15,6 +17,28 @@ from plone.dexterity.content import Container
 from collective.qna import _
 
 
+class AnswersVocab(object):
+    implements(IVocabularyFactory)
+    """Make a vocabulary of current answer
+    """
+
+    def __new__(self, context):
+        """Turn a content listing into a vocabulary"""
+        def shorten(s):
+            if len(s) > 20:
+                s = s[0:20] + '...'
+            s = re.compile('\s+').sub(' ', s)
+            return s
+
+        listing = context.restrictedTraverse('@@folderListing')()
+        return SimpleVocabulary([
+            SimpleVocabulary.createTerm(item.id,
+                                        item.id,
+                                        shorten(item.answer))
+            for item in listing
+        ])
+
+
 class IForum(model.Schema):
     """The top of a QnA form
     """
@@ -25,11 +49,11 @@ class IQuestion(model.Schema):
     """A question in the QnA forum
     """
 
-    #TODO: Vocab for answers
     directives.omitted('best_answer')
     directives.no_omit(IDisplayForm, 'best_answer')
-    best_answer = schema.TextLine(
+    best_answer = schema.Choice(
         title=_(u'Best answer'),
+        vocabulary='collective.qna.answers',
         required=False)
 alsoProvides(IQuestion, interfaces.IFormFieldProvider)
 
