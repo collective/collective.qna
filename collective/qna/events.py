e02a45407f3ada21450044485ac0df7277899c38
Jamie Lentin <jm@lentin.co.uk>
2012-10-31 18:03:09 +0000

    Answers shouldn't interit local roles, only their owner can edit

--- collective/qna/events.py
+++ collective/qna/events.py
@@ -2,3 +2,9 @@ def IndexParent(object, event):
     """Index object's parent
     """
     object.__parent__.reindexObject()
+
+def DontInheritLocalRoles(object, event):
+    """Turn inheritance of roles off
+    """
+    object.__ac_local_roles_block__ = True
+    object.reindexObjectSecurity()
--- collective/qna/tests/test_permissions.py
+++ collective/qna/tests/test_permissions.py
@@ -1,3 +1,4 @@
+from AccessControl import getSecurityManager
 from AccessControl.unauthorized import Unauthorized
 from Products.CMFCore.utils import getToolByName
 
@@ -20,11 +21,54 @@ class PermissionsTest(IntegrationTestCase):
         login(portal, USER_A_ID)
         with self.assertRaisesRegexp(Unauthorized, 'qna_forum'):
             portal.invokeFactory(type_name="qna_forum", id="qna1")
-        portal['qna'].invokeFactory(type_name="qna_question", id="question")
-        portal['qna']['question'].invokeFactory(type_name="qna_answer"
-                                               , id="answer1")
+        portal['qna'].invokeFactory(type_name="qna_question", id="question-a")
+        portal['qna']['question-a'].invokeFactory(type_name="qna_answer"
+                                               , id="answer-a")
 
         # ...so can User B
         login(portal, USER_B_ID)
-        portal['qna']['question'].invokeFactory(type_name="qna_answer"
-                                               , id="answer2")
+        with self.assertRaisesRegexp(Unauthorized, 'qna_forum'):
+            portal.invokeFactory(type_name="qna_forum", id="qna1")
+        portal['qna'].invokeFactory(type_name="qna_question", id="question-b")
+        portal['qna']['question-a'].invokeFactory(type_name="qna_answer"
+                                               , id="answer-b")
+        portal['qna']['question-b'].invokeFactory(type_name="qna_answer"
+                                               , id="answer-b")
+
+        # User A owns it's own questions and answers, but not B's
+        login(portal, USER_A_ID)
+        user = getSecurityManager().getUser()
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']),
+            ['Owner', 'Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']['answer-a']),
+            ['Owner', 'Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']['answer-b']),
+            ['Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-b']),
+            ['Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-b']['answer-b']),
+            ['Member', 'Authenticated'])
+
+        # User B owns it's own questions and answers, but not A's
+        login(portal, USER_B_ID)
+        user = getSecurityManager().getUser()
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']),
+            ['Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']['answer-a']),
+            ['Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-a']['answer-b']),
+            ['Owner', 'Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-b']),
+            ['Owner', 'Member', 'Authenticated'])
+        self.assertEquals(
+            user.getRolesInContext(portal['qna']['question-b']['answer-b']),
+            ['Owner', 'Member', 'Authenticated'])
